35d7b33fa0f9d5cf7f77dc98aa99e86a893a984b
omar issa11 <omom2168@gmail.com>
2026-06-21 12:57:43 +0300

    feat: add createpost endpoint

--- controllers/postscontrollers.js
+++ controllers/postscontrollers.js
@@ -3,3 +3,10 @@ const getAllPosts = async (req, res) => {
   const posts = Posts.find({ createdBy: req.user.userId }).sort("-createdAt");
   res.status(200).json({ posts });
 };
+
+const createPost = async (req, res) => {
+  const { content, photo, video } = req.body;
+  const post = await Posts.create({content, photo,video, postedBy: req.user.userId});
+  return res.status(201).json({post})
+}
+
